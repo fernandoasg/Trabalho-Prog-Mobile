7abc46f55a375bf7ec9eab9cc307f6befa7c18f6
f3rnand0_lp@hotmail.com <f3rnand0_lp@hotmail.com>
2019-10-06 15:46:54 -0300

    Iniciando a implementar o "banco de dados" com componentes Room + ViewModel + LiveData

--- LearnCards/app/src/main/java/com/example/learncards/Entities/User.java
+++ LearnCards/app/src/main/java/com/example/learncards/Entities/User.java
@@ -1,17 +1,43 @@
 package com.example.learncards.Entities;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "user")
 public class User {
 
+    @PrimaryKey(autoGenerate = true)
     private long id;
+
     private String name;
+
     private String email;
+
     private String password;
 
-    public User(long i, String n, String e, String p){
-        this.id = i;
-        this.name = n;
-        this.email = e;
-        this.password = p;
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
