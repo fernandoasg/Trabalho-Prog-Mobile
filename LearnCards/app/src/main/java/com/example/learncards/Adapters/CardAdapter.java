91c2d7b0a3a1a57adb4812ede3f16a5ae615a4d1
f3rnand0_lp@hotmail.com <f3rnand0_lp@hotmail.com>
2019-10-13 20:44:00 -0300

    Iniciando RecyclerView

---
+++ LearnCards/app/src/main/java/com/example/learncards/Adapters/CardAdapter.java
@@ -0,0 +1,41 @@
+package com.example.learncards.Adapters;
+/*
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.learncards.R;
+
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardHolder> {
+
+    @NonNull
+    @Override
+    public CardHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardHolder cardHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class CardHolder extends RecyclerView.ViewHolder{
+        private TextView textViewTitle;
+        private TextView textViewSubject;
+
+        public CardHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.text_view_card_title);
+            textViewSubject = itemView.findViewById(R.id.text_view_card_subject);
+
+        }
+    }
+}
+*/
--- LearnCards/app/src/main/java/com/example/learncards/LoginActivity.java
+++ LearnCards/app/src/main/java/com/example/learncards/LoginActivity.java
@@ -1,19 +1,36 @@
 package com.example.learncards;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.learncards.Entities.User;
+import com.example.learncards.ViewModel.UserViewModel;
+
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
 
     private Button loginButton;
+    private UserViewModel userViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
+        userViewModel.getAllUsers().observe(this, new Observer<List<User>>() {
+            @Override
+            public void onChanged(@Nullable List<User> users) {
+                //update RecycleView
+            }
+        });
 
         loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
