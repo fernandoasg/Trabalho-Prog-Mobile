644f920fd211b54ed92e9c01fc4aa59941591f1c
Fernando Augusto <f3rnand0_lp@hotmail.com>
2019-11-20 15:16:41 -0300

    Mostrando se acertou ou errou

--- LearnCards/app/src/main/java/com/example/learncards/Activities/QuestionsFragment.java
+++ LearnCards/app/src/main/java/com/example/learncards/Activities/QuestionsFragment.java
@@ -2,6 +2,7 @@ package com.example.learncards.Activities;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Gravity;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +33,7 @@ public class QuestionsFragment extends Fragment {
 
     private TextView questionText;
     private TextView questionDescription;
+    private RadioGroup radioGroup;
     private RadioButton answerARadio;
     private RadioButton answerBRadio;
     private RadioButton answerCRadio;
@@ -42,6 +45,7 @@ public class QuestionsFragment extends Fragment {
     private int lastStep = 0;
     private int currentQuestion = 0;
     private long cardId = 0;
+    private char userCurrentAnswer = 'f';
 
     final Handler handler = new Handler();
 
@@ -52,11 +56,42 @@ public class QuestionsFragment extends Fragment {
 
         questionText = view.findViewById(R.id.questionText);
         questionDescription = view.findViewById(R.id.questionDescription);
+        radioGroup = view.findViewById(R.id.radioGroup);
         answerARadio = view.findViewById(R.id.answerA);
+        answerARadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userCurrentAnswer = 'a';
+            }
+        });
         answerBRadio = view.findViewById(R.id.answerB);
+        answerBRadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userCurrentAnswer = 'b';
+            }
+        });
         answerCRadio = view.findViewById(R.id.answerC);
+        answerCRadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userCurrentAnswer = 'c';
+            }
+        });
         answerDRadio = view.findViewById(R.id.answerD);
+        answerDRadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userCurrentAnswer = 'd';
+            }
+        });
         answerERadio = view.findViewById(R.id.answerE);
+        answerERadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userCurrentAnswer = 'e';
+            }
+        });
 
         nextFinishButton = view.findViewById(R.id.nextQuestionButton);
 
@@ -87,30 +122,88 @@ public class QuestionsFragment extends Fragment {
         nextFinishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkAnswer();
-                nextFinishButton.setActivated(false);
+                boolean canContinue = checkAnswer();
 
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        currentStep += 1;
-                        currentQuestion += 1;
-                        checkStepAndChosseQuestion();
-                        increaseProgressBar(finalStepAmount);
-                    }
-                }, 3000);
+                if(canContinue){
+                    nextFinishButton.setActivated(false);
+
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            currentStep += 1;
+                            currentQuestion += 1;
+                            checkStepAndChosseQuestion();
+                            increaseProgressBar(finalStepAmount);
+                        }
+                    }, 3000);
+                }
             }
         });
 
         return view;
     }
 
-    private void checkAnswer(){
-        System.out.println("Verificar se acertou ou errou");
-        System.out.println("Verificar se acertou ou errou");
-        System.out.println("Verificar se acertou ou errou");
-        System.out.println("Verificar se acertou ou errou");
-        ShowResult("Mostra se acertou ou errou");
+    private boolean checkAnswer(){
+
+        int selectId = radioGroup.getCheckedRadioButtonId();
+        if(selectId != -1){
+
+            char writeAnswer = questions.get(currentQuestion).getAnswer();
+            boolean acertou = writeAnswer == userCurrentAnswer;
+            if(acertou){
+                trocaCor(userCurrentAnswer, true);
+                ShowResult("Certa resposta!!!");
+            }else{
+                trocaCor(userCurrentAnswer, false);
+                trocaCor(writeAnswer, true);
+                ShowResult("Resposta incorreta!!!");
+            }
+
+            return true;
+        }else{
+            ShowResult("Você precisa escolher uma alternativa!");
+            return false;
+        }
+    }
+
+    private void trocaCor(char op, boolean certo){
+        if(op == 'f'){
+            answerARadio.setTextColor(Color.BLACK);
+            answerBRadio.setTextColor(Color.BLACK);
+            answerCRadio.setTextColor(Color.BLACK);
+            answerDRadio.setTextColor(Color.BLACK);
+            answerERadio.setTextColor(Color.BLACK);
+        }else if(op == 'a'){
+            if(certo){
+                answerARadio.setTextColor(Color.GREEN);
+            }else{
+                answerARadio.setTextColor(Color.RED);
+            }
+        }else if(op == 'B'){
+            if(certo){
+                answerBRadio.setTextColor(Color.GREEN);
+            }else{
+                answerBRadio.setTextColor(Color.RED);
+            }
+        }else if(op == 'c'){
+            if(certo){
+                answerCRadio.setTextColor(Color.GREEN);
+            }else{
+                answerCRadio.setTextColor(Color.RED);
+            }
+        }else if(op == 'd'){
+            if(certo){
+                answerDRadio.setTextColor(Color.GREEN);
+            }else{
+                answerDRadio.setTextColor(Color.RED);
+            }
+        }else if(op == 'e'){
+            if(certo){
+                answerERadio.setTextColor(Color.GREEN);
+            }else{
+                answerERadio.setTextColor(Color.RED);
+            }
+        }
     }
 
     private void ShowResult(String message){
@@ -139,6 +232,8 @@ public class QuestionsFragment extends Fragment {
         }else if(currentStep == lastStep){
             nextFinishButton.setText("Finalizar");
         }
+        trocaCor('f', true);
+        radioGroup.clearCheck();
         nextFinishButton.setActivated(true);
 
         loadQuestions();
