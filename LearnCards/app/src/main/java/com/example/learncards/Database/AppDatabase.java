3490c282fb6a3f5625a46cd16e424e99d7d90d52
VitAndrGui <vitor.guidorizzi@hotmail.com>
2019-11-21 17:05:22 -0400

    DB seeder pra apresentação

--- LearnCards/app/src/main/java/com/example/learncards/Database/AppDatabase.java
+++ LearnCards/app/src/main/java/com/example/learncards/Database/AppDatabase.java
@@ -82,24 +82,12 @@ public abstract class AppDatabase extends RoomDatabase {
 
         @Override
         protected Void doInBackground(Void... voids) {
+
             userDao.insert(new User("Administrador", "admin@gmail.com", "123"));
 
+//---------------------------- PROG PARA DISPOSITIVOS MÓVEIS, NOTIFICAÇÃO --------------------------
+
             subjectDao.insert(new Subject("Programação para Dispositivos Móveis", "Notificação"));
-
-            subjectDao.insert(new Subject("Biologia", "Sistema Cardiovascular"));
-            subjectDao.insert(new Subject("Biologia", "Espécies"));
-
-            subjectDao.insert(new Subject("Física", "Leis de Newton"));
-            subjectDao.insert(new Subject("Física", "Resistencia do Ar"));
-
-            subjectDao.insert(new Subject("Quimica", "Reações quimicas"));
-            subjectDao.insert(new Subject("Quimica", "Atomos"));
-
-            subjectDao.insert(new Subject("Matemática", "Equações de 2nd grau"));
-            subjectDao.insert(new Subject("Matemática", "Algebra Linear"));
-
-            subjectDao.insert(new Subject("Filosofia", "Maconha I"));
-            subjectDao.insert(new Subject("Filosofia", "Marxismo II"));
 
             cardDao.insert(new Card(1, 1, "O que é uma Notificação",
                     "Uma notificação é uma mensagem que pode ser exibida ao usuário fora da IU normal do aplicativo.\n" +
@@ -108,23 +96,6 @@ public abstract class AppDatabase extends RoomDatabase {
                             "A área de notificação e a gaveta de notificação podem ser visualizadas pelo usuário a qualquer momento.\n",
                     "Podemos usar uma notificação para lembrar os usuários de fazer algo, para avisar que algo aconteceu, como " +
                             "o recebimento de um e-mail ou uma promoção ativa no aplicativo.", 0));
-            cardDao.insert(new Card(2, 1, "Ações de Notificação",
-                    "Deve-se adicionar pelo menos uma ação à notificação.\n" +
-                            "Uma ação permite que os usuários direcionem-se diretamente da notificação para uma Activity.\n" +
-                            "Onde podem visualizar um ou mais eventos ou realizar outros trabalhos.\n" +
-                            "Deve-se sempre definir a ção que será ativada quando o usuário clicar na notificação.\n" +
-                            "Geralmente, esta ação abre uma Activity no aplicativo.\n",
-                    "Caso o usuário receba uma notificação de um email que foi recebido, a ação de clicar " +
-                            "na notificação pode, por exemplo, abrir o aplicativo de email na Activity em que é " +
-                            "mostrado o email com mais detalhes.", 0));
-            cardDao.insert(new Card(3, 1, "Como usar notificações",
-                     "Aqui vai a descrição a da aula",
-                    "Aqui vai uma forma de usar", 0));
-            cardDao.insert(new Card(4, 2, "Sistema Cardiovascular ENEM", "Perguntas sobre o sistema cardiovascular p/ o ENEM", "contexto ?", 0));
-            cardDao.insert(new Card(5, 3, "Espécies reino animal", "Perguntas espécies do reino animal", "contexto ?", 0));
-            cardDao.insert(new Card(6, 4, "Gravidade questões práticas", "Perguntas sobre a lei de newton mais famosa !", "contexto ?", 0));
-            cardDao.insert(new Card(7, 5, "Resistencia do Ar, ENEM", "Perguntas sobre a resistencia do ar p/ o ENEM", "contexto ?", 0));
-
 
             questionDao.insert(new Question(1, 1, "O que é uma Notificação?",
                     "É uma mensagem que pode ser exibida ao usuário fora da IU do aplicativo",
@@ -140,6 +111,84 @@ public abstract class AppDatabase extends RoomDatabase {
                     "Tirar foto de forma mais rápida",
                     "Mostrar as últimas mensagens recebidas do WhatsApp",
                     'd'));
+
+            cardDao.insert(new Card(2, 1, "Ações de Notificação",
+                    "Deve-se adicionar pelo menos uma ação à notificação.\n" +
+                            "Uma ação permite que os usuários direcionem-se diretamente da notificação para uma Activity.\n" +
+                            "Onde podem visualizar um ou mais eventos ou realizar outros trabalhos.\n" +
+                            "Deve-se sempre definir a ção que será ativada quando o usuário clicar na notificação.\n" +
+                            "Geralmente, esta ação abre uma Activity no aplicativo.\n",
+                    "Caso o usuário receba uma notificação de um email que foi recebido, a ação de clicar " +
+                            "na notificação pode, por exemplo, abrir o aplicativo de email na Activity em que é " +
+                            "mostrado o email com mais detalhes.", 0));
+
+            questionDao.insert(new Question(3, 2, "O que é ação de notificação?",
+                    "Os botões abaixo do corpo da notificação que disparão intents quando selecionados",
+                    "A ação feita pelo aplicativo antes de disparar a notificação",
+                    "A ação feita pelo aplicativo após disparar a notificação",
+                    "Apenas os botões encapsulados no corpo da notificação",
+                    "Apenas o intent disparado ao selecionar o botão encapsulado no corpo da notificação",
+                    'a'));
+
+            questionDao.insert(new Question(4, 2, "Podemos ações de notificações para as seguintes ações, exceto:",
+                    "Disponibilizar várias ações para o usuário como abrir o app ou marcar uma mensagem como lida",
+                    "Desinstalar o aplicativo",
+                    "Abrir o aplicativo automaticamente, sem autorização do usuário",
+                    "Reativar Threads em waiting ou blocked",
+                    "Gabaritar prog mobile",
+                    'a'));
+
+
+//---------------------------- Biologia, Sistema Cardiovascular --------------------------
+
+            subjectDao.insert(new Subject(2,"Biologia", "Sistema Cardiovascular"));
+
+            cardDao.insert(new Card(3, 2, "Sistema Cardiovascular",
+                    "O sistema cardiovascular ou sistema circulatório humano é responsável pela circulação do sangue, de modo a transportar os nutrientes e o oxigênio por todo o corpo. O Sistema Cardiovascular é formado pelos vasos sanguíneos e o coração.",
+                    "Não há um exemplo para este card",
+                    0));
+
+            questionDao.insert(new Question(5, 3, "O sistema cardiovascular NÃO é responsavel por",
+                    "Realizar a circulação do sangue",
+                    "Transportar os nutrientes e o oxigênio por todo o corpo",
+                    "Distribuição de nutrientes para as células e coleta de suas excretas metabólicas para serem eliminadas por órgãos excretores",
+                    "Realizar a sensação de cócegas no pé",
+                    "Transportar hormônios por todo o corpo",
+                    'd'));
+
+            questionDao.insert(new Question(6, 3, "Tudo a seguir faz parte do sistema cardiovascular, exceto:",
+                    "Coração",
+                    "Vasos sanguíneos",
+                    "Vasos linfáticos",
+                    "Sangue",
+                    "Papilas gustativas",
+                    'e'));
+
+//---------------------------- Outros --------------------------
+
+            subjectDao.insert(new Subject(3, "Biologia", "Espécies"));
+
+            subjectDao.insert(new Subject(4, "Física", "Leis de Newton"));
+            subjectDao.insert(new Subject(5, "Física", "Resistencia do Ar"));
+
+            subjectDao.insert(new Subject(6, "Quimica", "Reações quimicas"));
+            subjectDao.insert(new Subject(7, "Quimica", "Atomos"));
+
+            subjectDao.insert(new Subject(8, "Matemática", "Equações de 2nd grau"));
+            subjectDao.insert(new Subject(9, "Matemática", "Algebra Linear"));
+
+            subjectDao.insert(new Subject(10, "Filosofia", "Comunismo I"));
+            subjectDao.insert(new Subject(11, "Filosofia", "Marxismo II"));
+
+            cardDao.insert(new Card(5, 3, "Espécies reino animal", "Perguntas espécies do reino animal", "contexto ?", 0));
+            cardDao.insert(new Card(6, 4, "Gravidade questões práticas", "Perguntas sobre a lei de newton mais famosa !", "contexto ?", 0));
+            cardDao.insert(new Card(7, 5, "Resistencia do Ar, ENEM", "Perguntas sobre a resistencia do ar p/ o ENEM", "contexto ?", 0));
+            cardDao.insert(new Card(8, 6, "Reações quimicas, ENEM", "Perguntas sobre a Reações quimicas p/ o ENEM", "contexto ?", 0));
+            cardDao.insert(new Card(9, 7, "Atomos, ENEM", "Perguntas sobre a Atomos p/ o ENEM", "contexto ?", 0));
+            cardDao.insert(new Card(10, 8, "Equações de 2nd grau, ENEM", "Perguntas sobre a Equações de 2nd grau p/ o ENEM", "contexto ?", 0));
+            cardDao.insert(new Card(11, 9, "Algebra Linear, ENEM", "Perguntas sobre a Algebra Linear p/ o ENEM", "contexto ?", 0));
+            cardDao.insert(new Card(12, 10, "Comunismo I, ENEM", "Perguntas sobre a Comunismo I p/ o ENEM", "contexto ?", 0));
+            cardDao.insert(new Card(13, 11, "Marxismo II, ENEM", "Perguntas sobre a Marxismo II p/ o ENEM", "contexto ?", 0));
 
             return null;
         }
